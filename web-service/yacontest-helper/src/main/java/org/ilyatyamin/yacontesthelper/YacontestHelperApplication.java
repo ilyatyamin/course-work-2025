bcbd52bcd4d1767a5d91da731ec7e932e47ffac5
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-18 19:30:45 +0300

    [init] add Spring project

---
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/YacontestHelperApplication.java
@@ -0,0 +1,13 @@
+package org.ilyatyamin.yacontesthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YacontestHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YacontestHelperApplication.class, args);
+    }
+
+}
---
+++ web-service/yacontest-helper/src/test/java/org/ilyatyamin/yacontesthelper/YacontestHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilyatyamin.yacontesthelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YacontestHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
