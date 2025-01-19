be1f8110661a1ad4519886aa378a5d1c204a263f
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-19 23:06:15 +0300

    [feat] add exceptions

---
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/exceptions/BrokenJsonException.java
@@ -0,0 +1,7 @@
+package org.ilyatyamin.yacontesthelper.exceptions;
+
+public class BrokenJsonException extends RuntimeException {
+    public BrokenJsonException(String message) {
+        super(message);
+    }
+}
---
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package org.ilyatyamin.yacontesthelper.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
