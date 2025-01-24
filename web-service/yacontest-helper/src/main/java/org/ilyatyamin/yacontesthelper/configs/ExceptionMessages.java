032ff59645074f645fde8d11d076ab87adb4e7bb
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-24 17:43:43 +0300

    [feat] update ExceptionMessages class

--- web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/configs/ExceptionMessages.java
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/configs/ExceptionMessages.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum ExceptionMessages {
-    YACONTEST_WRONG_KEY("Provided wrong yandex auth key.");
+    YACONTEST_WRONG_KEY("Provided wrong yandex auth key."),
+    EMPTY_BODY("YaContest provided empty body."),
+    NO_ACCESS_TO_CONTEST("No access to this contest."),
+    CONTEST_NOT_FOUND("Contest with this id not found."),;
 
     private final String message;
 }
