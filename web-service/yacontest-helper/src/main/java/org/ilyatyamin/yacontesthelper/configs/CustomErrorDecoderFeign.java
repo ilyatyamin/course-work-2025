10879d8a81522fee0fc99f96537f282f21819cb2
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-26 18:07:21 +0300

    [feat!] create handler for incorrect responses for feign client

---
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/configs/CustomErrorDecoderFeign.java
@@ -0,0 +1,20 @@
+package org.ilyatyamin.yacontesthelper.configs;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.ilyatyamin.yacontesthelper.exceptions.YaContestException;
+import org.springframework.http.HttpStatus;
+
+public class CustomErrorDecoderFeign implements ErrorDecoder {
+    @Override
+    public Exception decode(String s, Response response) {
+        if (response.status() == 401) {
+            return new YaContestException(HttpStatus.UNAUTHORIZED.value(), ExceptionMessages.YACONTEST_WRONG_KEY.name());
+        } else if (response.status() == 403) {
+            return new YaContestException(HttpStatus.FORBIDDEN.value(), ExceptionMessages.NO_ACCESS_TO_CONTEST.name());
+        } else if (response.status() == 404) {
+            return new YaContestException(HttpStatus.NOT_FOUND.value(), ExceptionMessages.CONTEST_NOT_FOUND.name());
+        }
+        return new YaContestException(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.body().toString());
+    }
+}
