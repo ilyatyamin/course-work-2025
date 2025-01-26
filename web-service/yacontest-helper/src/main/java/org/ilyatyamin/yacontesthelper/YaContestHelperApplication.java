4184ae5215307992a2fe45a437c9a7b8aa425f42
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-26 16:46:44 +0300

    [feat] add EnableFeignClients annotation to main application class

--- web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/YaContestHelperApplication.java
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/YaContestHelperApplication.java
@@ -2,9 +2,10 @@ package org.ilyatyamin.yacontesthelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
+@EnableFeignClients
 public class YaContestHelperApplication {
 
     public static void main(String[] args) {
