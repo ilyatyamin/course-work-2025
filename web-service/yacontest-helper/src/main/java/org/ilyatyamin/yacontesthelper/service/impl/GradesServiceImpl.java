d71c6bc55b507b837ade5ac27ce60485642e26ac
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-26 18:10:33 +0300

    [feat!] update getGradesList method

--- web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/service/impl/GradesServiceImpl.java
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/service/impl/GradesServiceImpl.java
@@ -1,17 +1,42 @@
 package org.ilyatyamin.yacontesthelper.service.impl;
 
+import lombok.AllArgsConstructor;
+import org.ilyatyamin.yacontesthelper.dao.GradesResult;
 import org.ilyatyamin.yacontesthelper.dto.grades.GradesRequest;
 import org.ilyatyamin.yacontesthelper.dto.grades.GradesResponse;
+import org.ilyatyamin.yacontesthelper.dto.yacontest.ContestSubmission;
 import org.ilyatyamin.yacontesthelper.repository.GradesResultRepository;
 import org.ilyatyamin.yacontesthelper.service.GradesService;
+import org.ilyatyamin.yacontesthelper.service.SubmissionProcessorService;
+import org.ilyatyamin.yacontesthelper.service.YaContestService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Service
 public class GradesServiceImpl implements GradesService {
     private GradesResultRepository gradesResultRepository;
 
+    private YaContestService yaContestService;
+    private SubmissionProcessorService submissionProcessorService;
+
     @Override
     public GradesResponse getGradesList(GradesRequest gradesRequest) {
-        return null;
+        List<String> problemList = yaContestService.getListOfProblems(gradesRequest.contestId(), gradesRequest.yandexKey());
+        List<ContestSubmission> submissionList = yaContestService.getSubmissionList(gradesRequest.contestId(), gradesRequest.yandexKey());
+
+        var resultTable = submissionProcessorService.processSubmissionList(
+                submissionList,
+                problemList,
+                gradesRequest.participants()
+        );
+
+        // TODO: после регистрации допилить userId
+        GradesResult result = gradesResultRepository.saveAndFlush(
+                new GradesResult(null, resultTable.toString())
+        );
+
+        return new GradesResponse(result.getId(), resultTable);
     }
 }
