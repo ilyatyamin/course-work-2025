3315d483399fb381479f9b636a041d0b3723d2f1
Ilya Tyamin <tyaminilya@gmail.com>
2025-01-26 17:47:13 +0300

    [feat] update GradesResultRepository

--- web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/repository/GradesResultRepository.java
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/repository/GradesResultRepository.java
@@ -2,6 +2,8 @@ package org.ilyatyamin.yacontesthelper.repository;
 
 import org.ilyatyamin.yacontesthelper.dao.GradesResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GradesResultRepository extends JpaRepository<GradesResult, Long> {
 }
