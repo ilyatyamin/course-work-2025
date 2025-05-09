aeab372c71b03b4dbecf4a951b20885cfb5823e8
Ilya Tyamin <tyaminilya@gmail.com>
2025-05-09 19:47:01 +0300

    [feat][backend] fix exception

--- web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/error/YaContestException.kt
+++ web-service/yacontest-helper/src/main/java/org/ilyatyamin/yacontesthelper/error/YaContestException.kt
@@ -1,13 +1,3 @@
-package org.ilyatyamin.yacontesthelper.error;
+package org.ilyatyamin.yacontesthelper.error
 
-import lombok.Getter;
-
-@Getter
-public class YaContestException extends RuntimeException {
-    private final Integer code;
-
-    public YaContestException(Integer code, String message) {
-        super(message);
-        this.code = code;
-    }
-}
+class YaContestException(val code: Int, message: String?) : RuntimeException(message)
