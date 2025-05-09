88023e45dff2eb47109544885d09078edac5fc2a
Ilya Tyamin <tyaminilya@gmail.com>
2025-05-09 15:57:48 +0300

    [fix][devops] fix rerun backend shell script

--- rerun.sh
+++ rerun.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 # shellcheck disable=SC2164
-# shellcheck disable=SC2103
-docker-compose up -d database
-ls -la
 cd web-service/yacontest-helper
-source packager.sh
+mvn clean package
+# shellcheck disable=SC2103
 cd ..
-docker-compose down -v --remove-orphans
-docker images | grep -v postgres | awk '{print $3}' | xargs docker rmi
 docker-compose build --no-cache
-docker-compose up
+docker-compose up -d database
+docker-compose down -v --remove-orphans --volumes --rmi local -f database
+docker images | grep -v postgres | awk '{print $3}' | xargs docker rmi -f
+docker-compose up --build -d planner_tgbot
+docker-compose up -d
